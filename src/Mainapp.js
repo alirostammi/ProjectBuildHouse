55ea208515b314ba25018f1a57038176fa7af0a2
alirostammi <alireza.ros76@gmail.com>
2025-04-20 23:40:36 +0330

    Add files via upload

---
+++ src/Mainapp.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import './App.css'
+
+function Mainapp() {
+	
+	return (
+		<>
+			<Nav />
+			<Header />
+			<Body />
+			<Footer />
+		</>
+	)
+}
+
+export default Mainapp;
